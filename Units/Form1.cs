35c45fb7c21d1547d97f194be5ccf7807a08f0fb
David Petticrew <cardassianscot@gmail.com>
2018-01-25 14:00:21 +0000

    Added squared and cubed units

--- Units/Form1.cs
+++ Units/Form1.cs
@@ -24,6 +24,32 @@ namespace Units
             {"Gm", 1e9 }
         };
 
+        Dictionary<string, double> areas = new Dictionary<string, double>()
+        {
+            {"nm\xB2", 1e-18 },
+            {"μm\xB2", 1e-12 },
+            {"mm\xB2", 1e-6 },
+            {"cm\xB2", 1e-4 },
+            {"m\xB2", 1 },
+            {"km\xB2", 1e6 },
+            {"Mm\xB2", 1e12 },
+            {"Gm\xB2", 1e18 }
+        };
+
+        Dictionary<string, double> volumes = new Dictionary<string, double>()
+        {
+            {"nm\xB3", 1e-27 },
+            {"μm\xB3", 1e-18 },
+            {"mm\xB3", 1e-9 },
+            {"cm\xB3", 1e-6 },
+            {"m\xB3", 1 },
+            {"km\xB3", 1e9 },
+            {"Mm\xB3", 1e18 },
+            {"Gm\xB3", 1e27 }
+        };
+
+        string type = "prefix";
+
         public Form1()
         {
             InitializeComponent();
@@ -32,6 +58,10 @@ namespace Units
                 comboBox1.Items.Add(k);
                 comboBox2.Items.Add(k);
             }
+            foreach (string k in areas.Keys)
+                comboBox1.Items.Add(k);
+            foreach (string k in volumes.Keys)
+                comboBox1.Items.Add(k);
             comboBox1.SelectedIndex = 4;
             comboBox2.SelectedIndex = 4;
         }
@@ -41,7 +71,13 @@ namespace Units
             try
             {
                 double d = double.Parse(textBox1.Text);
-                double d2 = d * prefixes[comboBox1.Text] / prefixes[comboBox2.Text];
+                double d2;
+                if (comboBox1.SelectedIndex < prefixes.Count)
+                    d2 = d * prefixes[comboBox1.Text] / prefixes[comboBox2.Text];
+                else if (comboBox1.SelectedIndex < 2 * prefixes.Count)
+                    d2 = d * areas[comboBox1.Text] / areas[comboBox2.Text];
+                else
+                    d2 = d * volumes[comboBox1.Text] / volumes[comboBox2.Text];
                 label1.Text = d2.ToString();
             }
             catch
@@ -57,6 +93,39 @@ namespace Units
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedIndex < prefixes.Count)
+            {
+                if (type != "prefix")
+                {
+                    comboBox2.Items.Clear();
+                    foreach (string k in prefixes.Keys)
+                        comboBox2.Items.Add(k);
+                    comboBox2.SelectedIndex = 4;
+                    type = "prefix";
+                }   
+            }
+            else if (comboBox1.SelectedIndex < 2 * prefixes.Count)
+            {
+                if (type != "area")
+                {
+                    comboBox2.Items.Clear();
+                    foreach (string k in areas.Keys)
+                        comboBox2.Items.Add(k);
+                    comboBox2.SelectedIndex = 4;
+                    type = "area";
+                }    
+            } 
+            else
+            {
+                if (type != "volume")
+                {
+                    comboBox2.Items.Clear();
+                    foreach (string k in volumes.Keys)
+                        comboBox2.Items.Add(k);
+                    comboBox2.SelectedIndex = 4;
+                    type = "volume";
+                }
+            }    
             update();
         }
 
